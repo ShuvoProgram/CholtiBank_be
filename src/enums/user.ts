7a71d102e12e5d3b3555f3c3c54bda5da53e9ce2
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-13 12:04:42 +0600

    Add ENUM_USER_ROLE enum.

---
+++ src/enums/user.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+export enum ENUM_USER_ROLE {
+  USER = 'user',
+  ADMIN = 'admin',
+  LOAN_OFFICER = 'loan_officer',
+  CUSTOMER_SERVICE_REPRESENTATIVE = 'customer_service_representative',
+}
